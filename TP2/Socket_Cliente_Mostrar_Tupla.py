1e8ef785c2b47708e4df9026969b3ca0bb604ce0
Javier Ouret <43023124+jaouret@users.noreply.github.com>
2023-05-29 11:02:14 -0300

    Add files via upload

--- TP2/Socket_Cliente_Mostrar_Tupla.py
+++ TP2/Socket_Cliente_Mostrar_Tupla.py
@@ -5,21 +5,22 @@ import sys
 # Creando un socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #addr = ('0.0.0.0', 0)
-#sock.bind(addr)
-print('Puerto:', sock)
-#socket.getaddrinfo("localhost", 80, proto=socket.IPPROTO_TCP)
+#sock.bind(addr) si fuese en servidor
+
+# Muestro tupla antes de la asignación de IP y puerto
+print('Datos de la conexión antes de completar datos en estructura:', sock)
+
 
 # Conecta el socket en el puerto cuando el servidor esté escuchando
 server_dir = ('localhost', 6667)
 print ('conectando a %s puerto %s',server_dir)
 sock.connect(server_dir)
-print('Puerto:', sock)
+# Muestro tupla luego de la asignación de IP y puerto
+print('Datos de la conexión luego de completar datos en estructura', sock)
 # Datos de la tupla
 
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
-#host, port = socket.getsockname()
-#print(socket.getsockname())
 print(f"Host: {hostname}")
 print(f"IP: {ip_address}")
 
