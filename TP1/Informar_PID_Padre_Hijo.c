fcfb4ef8abc91f458c2073b48a56d1fc54b893ea
Javier Ouret <43023124+jaouret@users.noreply.github.com>
2024-04-16 00:01:37 +0200

    Update Informar_PID_Padre_Hijo.c

--- TP1/Informar_PID_Padre_Hijo.c
+++ TP1/Informar_PID_Padre_Hijo.c
@@ -1,1 +1,28 @@
-
+// Bucle principal
+ while(1) {
+  // Aceptar una nueva conexión de la cola del listen
+  if ((nuevo_socket = accept(server_sd, (struct sockaddr *)&direccion,(socklen_t*)&longdir))<0) {
+            perror(“Falla accept");
+            exit(FALLA);
+        }
+        // Crear un nuevo proceso hijo para manejar la conexión
+        int pid = fork();
+        if (pid < 0) {
+                    perror(“Falla fork");
+                    exit(FALLA);
+        }
+        if (pid == 0) { // Identifico al Proceso hijo
+                    close(server_sd); // Cerrar el descriptor de archivo del servidor en el proceso hijo
+                    leovalor = read(nuevo_socket, buffer, BUFFER_SIZE);
+                    printf("Mensaje del cliente: %s\n", buffer);
+                    send(nuevo_socket, mensaje, strlen(mensaje), 0);
+                    printf("Respuesta enviada.\n");
+                    close(nuevo_socket); // Cerrar el socket en el proceso hijo
+                    exit(EPROCESO_EXITOSO);
+        } else { // Proceso padre
+                    close(nuevo_socket);} // Cerrar el socket en el proceso padre 
+           
+    }
+  }  // Cierro bucle principal
+        
+      
