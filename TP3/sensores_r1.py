f9853c4f0aa1dbe2055fc595cc2ca9038e4e9373
Javier Ouret <43023124+jaouret@users.noreply.github.com>
2024-05-27 21:29:10 -0300

    Update sensores_r1.py

--- TP3/sensores_r1.py
+++ TP3/sensores_r1.py
@@ -15,89 +15,98 @@ db = SQLAlchemy(app)
 
 app.app_context().push()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
 @app.route('/')
 def index():
-    users = User.query
+    users = User.query.all()
     return render_template('basic_table.html', users=users)
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
 
 class crear_dict(dict):
     # __init__ function
     def __init__(self):
-        self = dict()
+        super().__init__()
     # Function to add key:value
     def add(self, key, value):
         self[key] = value
 
-print('Defino credenciales de la base de datos ..')
-user = 'usuario'
-password = 'password'
-host = '127.0.0.1'
-port = 3306
-database = 'base_de_datos'
+def main():
+    print('Defino credenciales de la base de datos ...')
+    user = 'usuario'
+    password = 'password'
+    host = '127.0.0.1'
+    port = 3306
+    database = 'base_de_datos'
 
-print("Conecto con la base de datos utilizando mysql.connector...")
-cnx = mysql.connector.connect(user='',
-                              password='',
-                              host='127.0.0.1',
-                              port=puerto_local,
-                              database='')
-if cnx:
-    print ("Connectado")
-else:
-    print ("No conectado")
-
-cursor = cnx.cursor()
-query = "SELECT * FROM [table]"
-cursor.execute(query)
-records = cursor.fetchall()
-temp_ext, presion, humedad_ext, descripcion_clima = geo_latlon()
-print("Resultados= ",temp_ext, presion, humedad_ext, descripcion_clima)
-
-#datos del lugar
-while True:
-    try:
-        lugar=input("Lugar de la captura de los datos: ")
-        tipo_lugar=input("Tipo de lugar [au=abierto urbano] [an=abierto no urbano] [c=cerrado] ")
-        superficie=int(input("Superficie aproximada del lugar [m2]: "))
-        altura=int(input("Altura aproximada del lugar [m]: "))
-        presion_nm=presion
-        cant_capturas=int(input("Cantidad de capturas: "))
-        delta_t_capturas=int(input("Tiempo entre capturas (segs) : "))
-    except ValueError:
-        print("Error al ingresar datos...")
-        continue
+    print("Conecto con la base de datos utilizando mysql.connector...")
+    cnx = mysql.connector.connect(user=user,
+                                  password=password,
+                                  host=host,
+                                  port=port,
+                                  database=database)
+    if cnx:
+        print("Conectado")
     else:
-        break
-cont=0    
-while cont<cant_capturas:
-    cont=cont+1
-    verdadero = 1
-    if verdadero == 1:
-        print("Datos Disponibles!")
-        CO2_medido = random.uniform(250, 1100)
-        temp_sensor = random.uniform(temp_ext, temp_ext+10)
-        humedad_relativa = random.uniform(40, 80)
-        print("CO2: %d PPM" % CO2_medido)
-        print("Temperatura: %0.2f degrees C" % temp_sensor)
-        print("Humedad: %0.2f %% rH" % humedad_relativa)
+        print("No conectado")
+        return
 
-        d=datetime.now()
-        print("Fecha",d)
-        timestampStr = d.strftime("%d-%b-%Y (%H:%M:%S.%f)")
-        cursor = cnx.cursor()
-        query ="INSERT INTO data3(co2, temp, hum, fecha, lugar,altura,presion,presion_nm,temp_ext)\
-                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        record =(CO2_medido,temp_sensor,humedad_relativa,timestampStr,lugar,altura,presion,presion_nm,temp_ext)
-        cursor.execute(query,record)
-        cnx.commit()
-        print(cursor.rowcount, "Registro insertado..., acumulados:",cont,"\n")
-        time.sleep(delta_t_capturas)
-        print("\nEsperando nuevo registro de datos ...\n")
+    cursor = cnx.cursor()
+    query = "SELECT * FROM your_table"  # Reemplaza 'your_table' con el nombre de tu tabla
+    cursor.execute(query)
+    records = cursor.fetchall()
+    temp_ext, presion, humedad_ext, descripcion_clima = geo_latlon()
+    print("Resultados= ", temp_ext, presion, humedad_ext, descripcion_clima)
 
-cnx.close()
-print("Cierro conexión ...")
+    # datos del lugar
+    while True:
+        try:
+            lugar = input("Lugar de la captura de los datos: ")
+            tipo_lugar = input("Tipo de lugar [au=abierto urbano] [an=abierto no urbano] [c=cerrado] ")
+            superficie = int(input("Superficie aproximada del lugar [m2]: "))
+            altura = int(input("Altura aproximada del lugar [m]: "))
+            presion_nm = presion
+            cant_capturas = int(input("Cantidad de capturas: "))
+            delta_t_capturas = int(input("Tiempo entre capturas (segs) : "))
+        except ValueError:
+            print("Error al ingresar datos...")
+            continue
+        else:
+            break
 
+    cont = 0
+    while cont < cant_capturas:
+        cont += 1
+        verdadero = 1
+        if verdadero == 1:
+            print("Datos Disponibles!")
+            CO2_medido = random.uniform(250, 1100)
+            temp_sensor = random.uniform(temp_ext, temp_ext + 10)
+            humedad_relativa = random.uniform(40, 80)
+            print("CO2: %d PPM" % CO2_medido)
+            print("Temperatura: %0.2f degrees C" % temp_sensor)
+            print("Humedad: %0.2f %% rH" % humedad_relativa)
+
+            d = datetime.now()
+            print("Fecha", d)
+            timestampStr = d.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+            cursor = cnx.cursor()
+            query = ("INSERT INTO data3 (co2, temp, hum, fecha, lugar, altura, presion, presion_nm, temp_ext) "
+                     "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+            record = (CO2_medido, temp_sensor, humedad_relativa, timestampStr, lugar, altura, presion, presion_nm, temp_ext)
+            cursor.execute(query, record)
+            cnx.commit()
+            print(cursor.rowcount, "Registro insertado..., acumulados:", cont, "\n")
+            time.sleep(delta_t_capturas)
+            print("\nEsperando nuevo registro de datos ...\n")
+
+    cnx.close()
+    print("Cierro conexión ...")
+
+if __name__ == '__main__':
+    main()
