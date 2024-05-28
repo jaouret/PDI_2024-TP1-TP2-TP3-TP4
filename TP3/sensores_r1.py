3b8ec0d3f32ab1b4e8128c6f3e860649015a1f66
Javier Ouret <43023124+jaouret@users.noreply.github.com>
2024-05-27 21:14:17 -0300

    Update sensores_r1.py

--- TP3/sensores_r1.py
+++ TP3/sensores_r1.py
@@ -15,16 +15,6 @@ db = SQLAlchemy(app)
 
 app.app_context().push()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True)
-    age = db.Column(db.Integer, index=True)
-    address = db.Column(db.String(256))
-    phone = db.Column(db.String(20))
-    email = db.Column(db.String(120), index=True)
-
-db.create_all()
-
 @app.route('/')
 def index():
     users = User.query
